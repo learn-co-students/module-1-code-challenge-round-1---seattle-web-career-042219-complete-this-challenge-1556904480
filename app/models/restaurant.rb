c4e751af9be9f0af1129d7c5923ae2253c0e9f65
github username <github email address>
2019-05-03 11:32:11 -0700

    done

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,37 @@
 class Restaurant
+
   attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def customer
+    reviews.map do |review|
+      review.customer
+    end.uniq
+  end
+
+  def reviews
+    Review.all.select do |review|
+      review.restaurant == self
+    end
+  end
+
+  def average_star_rating
+    avg_star = reviews.map do |review|
+      review.rating
+    end
+    avg_star.sum/avg_star.count
+ end
+
+ 
 
 end
