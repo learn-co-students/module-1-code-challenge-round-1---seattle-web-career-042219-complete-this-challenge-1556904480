ed25db5d2e81f85e600b28b80f4627a58316a1b4
austenhasty <hastyae@gmail.com>
2019-05-03 11:33:00 -0700

    completed challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,28 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def customers
+    Review.all.map do |r|
+      r.customer
+    end.uniq
+  end
+
+  def reviews
+    Review.all.select do |r|
+      r.restaurant == self
+    end
+  end
+
 
 end
