3469576bbef34a993831c7ae7902171558fd4537
github username <github email address>
2019-05-03 11:32:11 -0700

    Done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,56 @@
 class Customer
   attr_reader :first_name, :last_name
 
+  @@all = []
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def add_review(restaurant, content, rating)
+    Review.new(self, restaurant, content, rating)
+  end
+
+  def num_reviews
+    Review.all.select{|reviews|
+       reviews.customer == self
+  }.count
+  end
+
+  def restaurants
+    Review.all.map{|reviews|
+       if reviews.customer == self
+          reviews.restaurant.name
+       end
+      }.uniq.compact
+  end
+
+  def self.find_by_name(name)
+    f_l_name = name.split
+    self.all.select{|customers|
+      customers.first_name == f_l_name[0] && customers.last_name == f_l_name[1]
+    }
+
+  end
+
+  def self.find_all_by_first_name(name)
+    self.all.select{|customers|
+  customers.first_name == name}
+  end
+
+  def self.all_names
+    self.all.select{|customers|
+    customers.first_name + customers.last_name
+    }
+  end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,29 @@
 class Review
-  
+  attr_accessor :restaurant, :content, :rating, :customer
+
+  @@all = []
+  def initialize(customer, restaurant, content, rating)
+    @customer = customer
+    @restaurant = restaurant
+    @content = content
+    @rating = rating
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+  #incomplete customer, stack level too deep currently
+#   def customer
+#     @@all.select{|reviews|
+#         if reviews == self
+#             reviews.customer
+#         end
+#     }
+#   end
+
+
 end
 
