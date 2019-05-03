72fc03d555265a66d3ebe96a2bd115489b377847
github username <github email address>
2019-05-03 11:31:13 -0700

    Done.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,42 @@
 class Restaurant
   attr_reader :name
 
-  def initialize(name)
+  @@all = []
+
+  def initialize(name:)
     @name = name
+  end
+
+  def customers
+    Review.all.collect { |element|
+      if element.restaurant == self
+        element.customer
+      end
+    }.uniq
+  end
+
+  def reviews
+    Review.all.collect { |element|
+      if element.restaurant == self
+        element
+      end
+    }
+  end
+
+  # def average_star_rating
+  #   self.reviews.collect { |review|
+  #     review.review
+  #   }
+  # end
+  #
+  # def self.find_by_name(rest_name)
+  #   Restaurant.all.find { |n|
+  #     n.name == rest_name
+  #   }
+  # end
+
+  def self.all
+    @@all
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,20 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+mike_honcho = Customer.new(first_name: "Mike", last_name: "Honcho")
+seymore_bu = Customer.new(first_name: "Seymore", last_name: "Bu")
+steve_smith = Customer.new(first_name: "Steve", last_name: "Smith")
+
+pizza_palace = Restaurant.new(name: "Pizza Palace")
+petes_sandwiches = Restaurant.new(name: "Pete's Sandwiches")
+soup_city = Restaurant.new(name: "Soup City")
+
+review1 = Review.new(customer: mike_honcho, restaurant: pizza_palace, review: "This place is legit yo. Stars: *****")
+review2 = Review.new(customer: mike_honcho, restaurant: petes_sandwiches, review: "This not so legit. Stars: ***")
+review3 = Review.new(customer: seymore_bu, restaurant: petes_sandwiches, review: "Delicious: ****")
+review4 = Review.new(customer: seymore_bu, restaurant: soup_city, review: "Not great: *")
+review5 = Review.new(customer: steve_smith, restaurant: soup_city, review: "Hate it: **")
+review6 = Review.new(customer: mike_honcho, restaurant: pizza_palace, review: "I been here before but this stuff smacks. Stars: *****")
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
