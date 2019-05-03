4c7ebc52bd564d89101482180119d1bcff79038e
Danny Tseng <github email address>
2019-05-03 11:33:23 -0700

    Done with code challenge

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,8 +1,45 @@
 class Restaurant
   attr_reader :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def reviews
+    Review.all.select {|review| review.restaurant == self}
+  end
+
+  def customers
+    reviews.map {|review| review.customer}.uniq
+  end
+
+  def average_star_rating
+    sum = 0.0
+    reviews.each {|review| sum += review.rating}
+    sum / reviews.length
+  end
+
+  def longest_review
+    target = ""
+    num = 0;
+    reviews.each do |review|
+      if num < review.content.length
+        target = review.content
+        num = review.content.length
+      end
+    end
+    target
+  end
+
+  def self.find_by_name(name)
+    @@all.find {|restaurant| restaurant.name == name}
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,20 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+customer1 = Customer.new("Danny", "Tseng")
+customer2 = Customer.new("Tony", "Stark")
+customer3 = Customer.new("Tony", "Ja")
+customer4 = Customer.new("Tony", "Stark")
+
+restaurant1 = Restaurant.new("McD")
+restaurant2 = Restaurant.new("DTF")
+
+customer1.add_review(restaurant1, "Very Very Very Good!!", 5)
+customer2.add_review(restaurant1, "So So!!", 2)
+customer2.add_review(restaurant2, "Not sure how I feel about this!!", 1)
+customer4.add_review(restaurant2, "Love this so much!!", 5)
+customer1.add_review(restaurant2, "Good Good!!", 3)
+customer3.add_review(restaurant2, "Don't care!", 2)
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
